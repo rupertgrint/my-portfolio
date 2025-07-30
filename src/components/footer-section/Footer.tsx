c7fbd24695fcbaf802af0726f3135d73402f2fb0
rupertgrint <joorohi@gmail.com>
2025-07-31 02:33:41 +1000

    refactor: fix overflowing text

--- src/components/footer-section/Footer.tsx
+++ src/components/footer-section/Footer.tsx
@@ -3,8 +3,8 @@
 export default function Footer() {
   return (
     <footer className='flex flex-col items-center mt-16 border-t py-10 px-2 md:px-10 border-gray-400'>
-      <div className='flex flex-col md:flex-row items-center pb-20'>
-        <p className='pr-2 pb-2 md:pb-0 font-museo text-lg font-bold '>
+      <div className='flex flex-col md:flex-row items-center justify-center pb-20'>
+        <p className='pr-2 pb-2 md:pb-0 font-museo text-sm md:text-lg font-bold '>
           Let’s connect! — open to collabs:
         </p>
         <a
